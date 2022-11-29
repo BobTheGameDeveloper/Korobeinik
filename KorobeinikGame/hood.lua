c5a24acb2e7cc57cb9b255c2c337f88acdbf5347
Bobby Williams <115984967+BobTheGameDeveloper@users.noreply.github.com>
2022-11-29 14:09:53 +0000

    Add files via upload

--- KorobeinikGame/hood.lua
+++ KorobeinikGame/hood.lua
@@ -1,15 +1,16 @@
+--require "main"
 function hoodload()
-    font = love.graphics.newFont('assets/TriodPostnaja.ttf', 32)
+    font = love.graphics.newFont("assets/TriodPostnaja.ttf", 43)
     h = 3
-    counterhood = love.graphics.newImage('assets/balalayka.png')
-    heart = love.graphics.newImage('assets/heart.png')
+    counterhood = love.graphics.newImage("assets/balalayka.png")
+    heart = love.graphics.newImage("assets/heart.png")
+    xh = 950
 end
 
 function hooddraw()
-
-    love.graphics.print("X : 10", font, 700, 60)
-
-    love.graphics.draw(counterhood, 650, 50, 0, 2, 2)
+    love.graphics.setColor(1,1,1,0.9)
+    love.graphics.print("X:"..bcounter, font, xh+60, 50)
+    love.graphics.draw(counterhood, xh, 50, 0, 2, 2)
     for i = 1, h, 1 do
         love.graphics.draw(heart, 10 + i * 40, 50, 0, 4, 4)
     end
